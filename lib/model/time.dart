b2e7cfe6d545ba42bcf46d3766eeb366152ea665
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-11 12:24:58 -0300

    Aumentando atributos da classe Time

--- lib/model/time.dart
+++ lib/model/time.dart
@@ -4,13 +4,17 @@
 class Time {
   String nome;
   String position;
+  String image; 
+  String initials; // sigla
   int wins;
   int losses;
   Time({
     required this.nome,
     required this.position,
+    required this.image,
+    required this.initials,
     required this.wins,
     required this.losses,
   });
- 
 }
+
