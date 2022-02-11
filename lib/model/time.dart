bde4bbc876449691b4f8bed0935e057bebaff7ff
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-11 14:07:36 -0300

    Corrigindo erros

--- lib/model/time.dart
+++ lib/model/time.dart
@@ -2,14 +2,14 @@
   Esta é uma classe que representa um time.
 */
 class Time {
-  String nome;
-  String position;
+  String name;
+  int position;
   String image; 
   String initials; // sigla
   int wins;
   int losses;
   Time({
-    required this.nome,
+    required this.name,
     required this.position,
     required this.image,
     required this.initials,
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
-import 'package:brazil_apostas/screens/home_screen.dart';
+// import 'package:brazil_apostas/screens/home_screen.dart';
+import 'package:brazil_apostas/screens/times_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Brazil Apostas',
+      title: 'Brazil Apostas',      
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const HomeScreen(),
+      home: const TimesScreen(),
     );
   }
 }
